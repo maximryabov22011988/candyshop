0942f68b06996d6b1ea3acb9a056dd1df3e06241
Maxim Ryabov <designmr2015@gmail.com>
2018-11-09 19:46:48 +0400

    Перерабатывает переключение табов (добавляет управление с клавиатуры)

--- js/goods.js
+++ js/goods.js
@@ -110,8 +110,13 @@ basketCardsListElement.classList.remove('goods__cards--empty');
 var orderForm = document.querySelector('#order');
 var deliveryCourierContent = orderForm.querySelector('.deliver__courier');
 var deliveryStoreContent = orderForm.querySelector('.deliver__store');
+var deliveryCourierInput = orderForm.querySelector('#deliver__courier');
+var deliveryStoreInput = orderForm.querySelector('#deliver__store');
 var paymentCardContent = orderForm.querySelector('.payment__card-wrap');
 var paymentCashContent = orderForm.querySelector('.payment__cash-wrap');
+var paymentCardInput = orderForm.querySelector('#payment__card');
+var paymentCashInput = orderForm.querySelector('#payment__cash');
+
 
 /**
  * Шаблоны
@@ -624,32 +629,40 @@ catalogCardsListElement.addEventListener('keydown', function (evt) {
 var toggleTab = function (evt) {
   var target = evt.target;
 
-  switch (target.id) {
+  if (target.tagName.toLowerCase() !== 'label') return;
+
+  switch (target.htmlFor) {
     case 'deliver__courier':
+      deliveryCourierInput.checked = true;
+      deliveryStoreInput.checked = false;
       deliveryCourierContent.classList.remove('visually-hidden');
       deliveryStoreContent.classList.add('visually-hidden');
       break;
     case 'deliver__store':
+      deliveryStoreInput.checked = true;
+      deliveryCourierInput.checked = false;
       deliveryStoreContent.classList.remove('visually-hidden');
       deliveryCourierContent.classList.add('visually-hidden');
       break;
     case 'payment__card':
+      paymentCardInput.checked = true;
+      paymentCashInput.checked = false;
       paymentCardContent.classList.remove('visually-hidden');
       paymentCashContent.classList.add('visually-hidden');
       break;
     case 'payment__cash':
+      paymentCardInput.checked = false;
+      paymentCashInput.checked = true;
       paymentCashContent.classList.remove('visually-hidden');
       paymentCardContent.classList.add('visually-hidden');
       break;
   }
-
 };
 
 orderForm.addEventListener('click', function (evt) {
   toggleTab(evt);
 });
 
-// Доработать переключение с клавиатуры!!!!!!!!!!!!
 orderForm.addEventListener('keydown', function (evt) {
   if (evt.which === KEYCODE_ENTER) {
     toggleTab(evt);
